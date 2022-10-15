5359f039a2029037f9946d211718622bc065b256
Viwe Nkepu <viwe.nkepu@hotmail.com>
2022-10-16 01:11:43 +0200

    Initial commit

---
+++ src/ASPNETDemo/OpenTelemetryConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using OpenTelemetry.Trace;
+using OpenTelemetry.Resources;
+using System.Diagnostics;
+
+namespace ASPNETDemo;
+
+internal static class OpenTelemetryConfiguration
+{
+    internal static string _serviceName = "Demo.Console";
+    internal static string _serviceVersion = "1.0.0";
+    internal static ActivitySource GetActivitySource()
+    {
+        // Define some important constants to initialize tracing with
+        return new ActivitySource(_serviceName, _serviceVersion);
+    }
+
+    internal static IServiceCollection AddOpenTelemetryTracingDemo(this IServiceCollection services)
+    {
+        // Configure important OpenTelemetry settings, the console exporter, and instrumentation library
+        services.AddOpenTelemetryTracing(tracerProvider =>
+        {
+            tracerProvider
+            .AddConsoleExporter()
+            .AddZipkinExporter(options =>
+            {
+                // not needed, it's the default
+                options.Endpoint = new Uri("http://localhost:9411/api/v2/spans");
+            })
+            .AddSource(_serviceName)
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(_serviceName, serviceVersion: _serviceVersion))
+            .AddHttpClientInstrumentation()
+            .AddAspNetCoreInstrumentation()
+            .AddSqlClientInstrumentation();
+        });
+
+        return services;
+    }
+}
---
+++ src/ASPNETDemo/Program.cs
@@ -0,0 +1,54 @@
+using ASPNETDemo;
+using System.Diagnostics;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddOpenTelemetryTracingDemo();
+
+var app = builder.Build();
+
+var activitySource = OpenTelemetryConfiguration.GetActivitySource();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    using var activity = activitySource.StartActivity("Start tag activity", ActivityKind.Internal);
+    activity?.SetTag("event 1 resource", new { Id = 1245, IsValid = true });
+    activity?.SetTag("event 1 state", "Pending");
+    activity?.SetTag("event 1 succeeded", true);
+
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateTime.Now.AddDays(index),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
